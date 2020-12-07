6774f454a1a1f1f9149c2a01c48a7ba091d74f8b
minhnguyen303 <minh09az09@gmail.com>
2020-12-07 10:23:28 +0700

    Upload bai thuc hanh Server App Show Time

---
+++ Server App Show Time/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1> Bây giờ là: <?php date_default_timezone_set('Asia/Ho_Chi_Minh') ?><?php echo date('Y-M-d h:m:s'); ?> </h1>
+</body>
+</html>
