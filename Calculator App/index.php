0ea4041f7e79a0b3f34c3056af874a51ef92b2af
minhnguyen303 <minh09az09@gmail.com>
2020-12-09 15:11:40 +0700

    Upload bai tap

--- Calculator App/index.php
+++ Calculator App/index.php
@@ -8,8 +8,39 @@
     <title>Document</title>
 </head>
 <body>
+<h1>Simple Calculator</h1>
 <form action="" method="post">
-
+    <input type="number" name="input1" placeholder="Số thứ nhất"><br>
+    <select name="operator">
+        <option value="+">Cộng</option>
+        <option value="-">Trừ</option>
+        <option value="*">Nhân</option>
+        <option value="/">Chia</option>
+    </select><br>
+    <input type="number" name="input2" placeholder="Số thứ hai"><br>
+    <button>Tính</button>
 </form>
 </body>
 </html>
+<?php
+function calc($num1, $operator, $num2){
+    switch ($operator){
+        case "+":
+            return $num1 + $num2;
+        case "-":
+            return $num1 - $num2;
+        case "*":
+            return $num1 * $num2;
+        case "/":
+            return $num1 / $num2;
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] == "POST"){
+    $num1 = $_POST['input1'];
+    $operator = $_POST['operator'];
+    $num2 = $_POST['input2'];
+
+    $result = calc($num1, $operator, $num2);
+    echo "Result: $result";
+}
